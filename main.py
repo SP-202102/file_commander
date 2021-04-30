7d8e4b74212db8087d7b66fdddfc3d6d9b6021f6
Frank Warstat <github.202102@smilingpirate.de>
2021-04-30 23:00:51 +0200

    added initial todos

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#todo 
+# todo 1 - list files and attributes (is directory, is cloud?)
+#
+# todo 2 - make folders to links
+#    how to display??
+#    inital: show on command line
+#    2nd? pygame?`
+#    3rd browser -> web server, python im WS installieren, deployen`
+# todo 3 - make links navigatable
+
+
+# todo create database
