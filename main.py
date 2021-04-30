5b7659506c5211d481d54b4f20222e47cbd4ec89
Frank Warstat <github.202102@smilingpirate.de>
2021-04-30 23:54:46 +0200

    initial file list created

--- main.py
+++ main.py
@@ -6,6 +6,15 @@
 #    2nd? pygame?`
 #    3rd browser -> web server, python im WS installieren, deployen`
 # todo 3 - make links navigatable
+from file_functions.file_list import file_list
+
+# todo move path to config -> later to db
+my_file_list = file_list( "L:\\" ) 
+
+files = my_file_list.get_file_objects()
+
+for f in files:
+	print( f.name )
 
 
 # todo create database
